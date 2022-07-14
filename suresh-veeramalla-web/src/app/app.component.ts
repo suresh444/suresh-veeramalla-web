342b2b88d0e632d7640585a7055cd60ff19b3373
suresh444 <sureshveeramalla4@gmail.com>
2022-07-14 21:18:36 +0530

    initial commit

--- suresh-veeramalla-web/src/app/app.component.ts
+++ suresh-veeramalla-web/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
+export interface PeriodicElement {
+  avatar_url:string,
+  login: string,
+  type: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
+  
   title = 'suresh-veeramalla-web';
+  searchValue:string
+  displayedColumns: string[] = [ 'avatar_url', 'login', 'type'];
+  dataSource = new MatTableDataSource<PeriodicElement>();
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private http:HttpClient){  }
+ 
+    getUserDate(){
+      const url= `https://api.github.com/search/users?q=${this.searchValue} in:login`
+      this.http.get(url).subscribe((res:any)=>{
+        this.dataSource =  new MatTableDataSource<PeriodicElement>(res.items);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      })
+    }
 }
--- suresh-veeramalla-web/src/app/app.module.ts
+++ suresh-veeramalla-web/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatTableModule ,
+    MatPaginatorModule,
+    HttpClientModule,
+    MatSortModule
+      
+
   ],
   providers: [],
   bootstrap: [AppComponent]
